90b72b9110a5e9b6e9db798856e5d44b53d692c7
n.zemlyanukhin <n.zemlyanukhin@robo.finance>
2020-12-29 00:51:13 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const micro = require('micro');
+const replies = require('./replies');
+
+const server = micro(async (req, res) => {
+  const { request, session } = await micro.json(req);
+  const response = session.new ? replies.welcome() : replies.repeatUserCommand(request.command);
+  return {
+    response,
+    version: '1.0',
+  };
+});
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
---
+++ src/replies.js
@@ -0,0 +1,23 @@
+exports.welcome = () => {
+  return {
+    response: {
+      text: 'Привет!',
+      tts: 'Привет, че как?.',
+      end_session: false,
+      buttons: [
+        { title: 'Ну привет!', hide: true },
+        { title: 'Как дела?', hide: true },
+      ],
+    },
+  };
+};
+
+/**
+ * @param {String} command
+ */
+exports.repeatUserCommand = command => {
+  return {
+    text: `${command}`,
+    end_session: false,
+   };
+};
