42b67e1569967b5694e79d167382ed104eea0565
n.zemlyanukhin <n.zemlyanukhin@robo.finance>
2020-12-29 00:55:14 +0700

    added dotenv, code style fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const micro = require('micro');
+const dotenv = require('dotenv');
 const replies = require('./replies');
 
+dotenv.config();
+
 const server = micro(async (req, res) => {
-  const { request, session } = await micro.json(req);
-  const response = session.new ? replies.welcome() : replies.repeatUserCommand(request.command);
-  return {
-    response,
-    version: '1.0',
-  };
+    const { request, session } = await micro.json(req);
+    const response = session.new ? replies.welcome() : replies.repeatUserCommand(request.command);
+
+    return {
+        response,
+        version: '1.0',
+    };
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT;
+
 server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
